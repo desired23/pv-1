2f746398fd62d60723044b67f35efd28fe52e793
desired23 <huydang2312003@gmail.com>
2023-08-07 16:22:34 +0700

    m

--- telex.js
+++ telex.js
@@ -1,20 +1,17 @@
 const input = "hfdawhwhcoomdd";
 
 function counter(input) {
- const tiengviet = ['aw', 'aa', 'dd', 'ee', 'oo', 'ow', 'w']
+ const tiengviet = new Set(['aw', 'aa', 'dd', 'ee', 'oo', 'ow', 'w']);
  let count = 0
  let i =0    
- if ( tiengviet.includes("w")) {
+ if ( tiengviet.has("w")) {
         count+=1
     }
  while (i < input.length) {
-const hehe = input.slice(i, i+2)
-    console.log(hehe);
-
-    if(tiengviet.includes(hehe)){
+    if(tiengviet.has(input.slice(i, i+2))){
         count+=1
-    }
-    i+=1
+        i+=2
+    }else i+=1
  }
  console.log(count);
 }
